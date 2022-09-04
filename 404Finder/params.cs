abfd5dba49be1b51e689b64090d567edde74914a
koko <github@koko-campus.com>
2022-09-04 23:24:30 +0900

    パラメタの取得。

---
+++ 404Finder/params.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+internal static partial class Program
+{
+	private static Dictionary<string, Dictionary<string, string>> args = new Dictionary<string, Dictionary<string, string>>()
+	{
+		{"-t", new Dictionary<string, string>() {
+			{"value", "10000" },
+			{"explanation", "timespan to wait for http response. (millisecond)" },
+		} },
+		{"-x", new Dictionary<string, string>() {
+			{"value", "OFF"},
+			{"explanation", "if set, no longer getting asked for confirmation."},
+		} },
+	};
+
+	private static bool importParams(string[] args)
+	{
+		for (int i = 1; i < args.Length; i++)
+		{
+			if (Regex.IsMatch(args[i], @"-(x)"))
+			{
+				args[i] = "ON";
+			}
+			else
+			{
+				args[i] = args[i + 1];
+				i++;
+			}
+
+		}
+		return true;
+	}
+
+}
+
