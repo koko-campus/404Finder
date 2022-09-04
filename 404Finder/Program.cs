08d86d8d88689d820244e3a2ee28f7a8af54c984
koko <github@koko-campus.com>
2022-09-04 23:23:02 +0900

    基礎(メイン)処理。

--- 404Finder/Program.cs
+++ 404Finder/Program.cs
@@ -5,7 +5,40 @@ internal static partial class Program
 	internal static int Main(string[] args)
 	{
 
+		var fstArg = args[0];
+
+		if (isUrl(fstArg))
+        {
+			urlWalker(fstArg);
+        } else if (File.Exists(fstArg))
+        {
+			var urls = File.ReadAllLines(fstArg).Where(line => isUrl(line)).ToList().ConvertAll(a => (dynamic)a);
+			if (urls.Count() == 0)
+            {
+				Console.WriteLine(" file you gave has no valid url inside. ");
+				Console.WriteLine(" show urls.txt to get infomation. ");
+				helper();
+				Environment.Exit(1);
+			}
+			looper(urls, (url, _) => urlWalker(url));
+        }
+		else
+        {
+			Console.WriteLine(" you sent invalid param(s). ");
+			Console.WriteLine(" show parameters explanation below. ");
+			helper();
+			Environment.Exit(1);
+        }
+
 		return 0;
 	}
+
+	private static void helper()
+    {
+		Console.WriteLine($" I receive parameters down below. ");
+		Console.WriteLine($" 1st param ->  url or path to file(read urls.txt to learn how to write). | required! ");
+		Console.WriteLine($" -t option -> set request timeout (millisecond) ");
+		Console.WriteLine($" -x option -> without any prompt. no value required, just put \"-t\" ");
+    }
 }
 
---
+++ 404Finder/urlWalker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal static partial class Program
+{
+    private static void urlWalker(string url)
+    {
+
+    }
+}
+
+
