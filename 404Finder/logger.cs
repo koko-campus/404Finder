a8262748d8243f2e1fcbf900c8bbd6b027f3081e
koko <github@koko-campus.com>
2022-09-05 10:53:51 +0900

    ログモジュールの作成。

---
+++ 404Finder/logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal static partial class Program
+{
+	private static List<string> _log = new();
+	private static List<string> _error = new();
+	private static void log(string message)
+	{
+		_log.Add(message);
+	}
+
+	private static void error(string message)
+	{
+		_error.Add(message);
+	}
+
+	private static void writeLog()
+	{
+		File.WriteAllLines(ROOT + ".log", _log);
+		File.WriteAllLines(ROOT + ".error", _error);
+	}
+}
+
